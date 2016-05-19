24e8b4efb25b1f9bead37cfbfac6fe62d3025997
bzz <whiteevilban@gmail.com>
2016-05-19 18:22:36 +0800

    insert

--- site/mybzz/crawler/MZCrawler.py
+++ site/mybzz/crawler/MZCrawler.py
@@ -4,29 +4,32 @@ import html
 from bs4 import BeautifulSoup
 
 from site.mybzz.domain.MzComment import MzComment
-
+from site.mybzz.util import DbUtil
 
 def getData():
     req = request.urlopen(
-        'http://app.flyme.cn/apps/public/evaluate/list?app_id=3072599&start=0&max=10')
+        'http://app.flyme.cn/apps/public/evaluate/list?app_id=3072599&start=0&max=10000')
 
     data = req.read().decode()
     print(data)
     value = json.loads(data)
     count = 0
     comment = MzComment()
+    conn,cur = DbUtil.getConn()
     for com in value['value']['list']:
-        print('第%d个评论' % count)
+        # print('第%d个评论' % count)
         comment.comment = html.unescape(com['comment'])
-        print('内容:', comment.comment)
+        # print('内容:', comment.comment)
         comment.time = com['create_time']
-        print('时间:', comment.time)
+        # print('时间:', comment.time)
         comment.author = html.unescape(com['user_name'])
-        print('作者:', comment.author)
+        # print('作者:', comment.author)
         comment.score = com['star']
-        print('评分:', comment.score)
+        # print('评分:', comment.score)
         count += 1
-        print()
+        print('INSERT INTO comment(game_name, content, comment_time, author, score) VALUES ("%s", "%s", "%s", "%s", %d);' % ('皇室战争',comment.comment,comment.time,comment.author,comment.score))
+        cur.execute('INSERT INTO comment(game_name, content, comment_time, author, score) VALUES ("%s", "%s", "%s", "%s", %d);' % ('皇室战争',comment.comment,comment.time,comment.author,comment.score))
+    conn.commit()
     comment.totalComCount = value['value']['totalCount']
     print(comment.totalComCount)
     # 获取总下载量和评分
--- site/mybzz/test/TestDb.py
+++ site/mybzz/test/TestDb.py
@@ -2,14 +2,18 @@ import pymysql
 
 from site.mybzz.util import DbUtil
 
-# conn = pymysql.connect(host="localhost", user="root", passwd="banban123", db="comment", port=3306, charset="utf8")
-#
-# cur = conn.cursor()
-#
-# cur.execute()
-statement = "select * from comment"
+conn = pymysql.connect(host="localhost", user="root", passwd="banban123", db="comment", port=3306, charset="utf8")
 
-data =DbUtil.getAllResult(statement)
+cur = conn.cursor()
 
-for d in data:
-    print("游戏名：%s,内容：%s,时间：%s" % (d[1],d[2],d[3]))
+cur.execute("INSERT INTO comment(game_name, content, comment_time, author, score)"
+            " VALUES ('游戏名123', '内容123', '2016-05-19 15:56:07', 'ban', '44');")
+# conn.commit()
+print("VALUES (%s, %s, %s, %s, %d);" % ('游戏名123', '内容123', '2016-05-19 15:56:07', 'ban', 44))
+
+# statement = "select * from comment"
+#
+# data =DbUtil.getAllResult(statement)
+
+# for d in data:
+#     print("游戏名：%s,内容：%s,时间：%s" % (d[1],d[2],d[3]))
