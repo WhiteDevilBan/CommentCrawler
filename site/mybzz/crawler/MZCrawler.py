e87679b65d70d757de605c579baa131154237482
bzz <whiteevilban@gmail.com>
2016-05-19 14:58:00 +0800

    new file

--- site/mybzz/crawler/MZCrawler.py
+++ site/mybzz/crawler/MZCrawler.py
@@ -3,30 +3,39 @@ from urllib import request
 import html
 from bs4 import BeautifulSoup
 
+from site.mybzz.domain.MzComment import MzComment
+
 
 def getData():
     req = request.urlopen(
-        'http://app.flyme.cn/apps/public/evaluate/list?app_id=3072599&start=0&max=1538')
+        'http://app.flyme.cn/apps/public/evaluate/list?app_id=3072599&start=0&max=10')
 
     data = req.read().decode()
     print(data)
     value = json.loads(data)
     count = 0
-    for comment in value['value']['list']:
+    comment = MzComment()
+    for com in value['value']['list']:
         print('第%d个评论' % count)
-        print('内容:', html.unescape(comment['comment']))
-        print('时间:', comment['create_time'])
-        print('作者:', html.unescape(comment['user_name']))
-        print('评分:', comment['star'])
+        comment.comment = html.unescape(com['comment'])
+        print('内容:', comment.comment)
+        comment.time = com['create_time']
+        print('时间:', comment.time)
+        comment.author = html.unescape(com['user_name'])
+        print('作者:', comment.author)
+        comment.score = com['star']
+        print('评分:', comment.score)
         count += 1
         print()
-
+    comment.totalComCount = value['value']['totalCount']
+    print(comment.totalComCount)
     # 获取总下载量和评分
     req2 = request.urlopen('http://app.flyme.cn/games/public/detail?package_name=com.supercell.clashroyale.mz')
     soup = BeautifulSoup(req2.read().decode('UTF-8'), "html.parser")
-    page = soup.prettify()
-    print('总评分：', soup.find('div', class_="star_bg").attrs['data-num'])
-    print("总下载量：", soup.find(text="下      载：").parent.next_sibling.next_sibling.string)
+    comment.totalScore = soup.find('div', class_="star_bg").attrs['data-num']
+    print('总评分：', comment.totalScore)
+    comment.totalDownload = soup.find(text="下      载：").parent.next_sibling.next_sibling.string
+    print("总下载量：", comment.totalDownload)
 
 
 if __name__ == '__main__':
---
+++ site/mybzz/domain/MzComment.py
@@ -0,0 +1,2 @@
+class MzComment(object):
+    pass
