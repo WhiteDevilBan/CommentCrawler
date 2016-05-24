a94633c59aa1eb998b6fbac9aeb568d3a837b196
bzz <whiteevilban@gmail.com>
2016-05-24 09:45:50 +0800

    xiaomi

--- site/mybzz/crawler/XiaoMiCrawler.py
+++ site/mybzz/crawler/XiaoMiCrawler.py
@@ -1,3 +1,5 @@
+import sys
+
 from site.mybzz.util import DbUtil
 from site.mybzz.util import BsUtil
 from site.mybzz.util import DateUtil
@@ -8,11 +10,12 @@ def getTop30():
     result = BsUtil.praseJson(
         'http://market.xiaomi.com/apm/toplist/15?clientId=2bb48bb54747e03a6ab667ab7b51050a&co=CN&la=zh&os=1461822601&page=%d&sdk=22&stamp=0' % page)
     # print(result)
-    for game in result['listApp']:
+    for game in result['listApp'][28:]:
         print('游戏名：%s，id：%s，总评分：%s' % (game['displayName'], game['id'], game['ratingScore']))
         try:
             getData(game['id'], game['displayName'], game['ratingScore'])
         except:
+            print(sys.exc_info()[0], ":", sys.exc_info()[1])
             pass
 
 
@@ -30,32 +33,38 @@ def getData(id, name, totalScore):
 
     print('INSERT INTO games(game_name,from_store, total_comment_count, total_score, total_download, data_date) '
           'VALUES ("%s", "%s", "%s", "%s", "%s", "%s");' % (
-              name, 'xiaomi', totalComCount, totalScore, 0, DateUtil.currentDate()))
-    # cur.execute('INSERT INTO games(game_name,from_store, total_comment_count, total_score, total_download, data_date) '
-    #             'VALUES ("%s", "%s", "%s", "%s", "%s", "%s");' % (name, 'xiaomi', totalComCount, totalScore, 0, DateUtil.currentDate()))
-    # game_id = cur.lastrowid
-    game_id = 0
+              name, 'xiaomi', totalComCount, totalScore * 10, 0, DateUtil.currentDate()))
+    cur.execute('INSERT INTO games(game_name,from_store, total_comment_count, total_score, total_download, data_date) '
+                'VALUES ("%s", "%s", "%s", "%s", "%s", "%s");' % (
+                    name, 'xiaomi', totalComCount, totalScore * 10, 0, DateUtil.currentDate()))
+    game_id = cur.lastrowid
+    # game_id = 0
     while (hasMore):
         result = BsUtil.praseJson('http://market.xiaomi.com/apm/comment/list/%s?'
                                   'clientId=2bb48bb54747e03a6ab667ab7b51050a&co=CN'
                                   '&la=zh&os=1461822601&page=%s&sdk=22' % (id, page))
         # print(result)
         for comment in result['comments']:
-            content = comment['commentValue'].replace("\"", "'")
+            content = comment['commentValue'].replace("\"", "'").replace(" ", "")
             score = comment['pointValue']
             time = comment['updateTime']
             author = comment['nickname'].replace("\"", "'")
-            # print(content, score, author, DateUtil.lomgToStrTime(time / 1000))
             # 插入评论
             try:
                 print('INSERT INTO comment(game_id, content, comment_time, author, score) '
-                      'VALUES ("%s", "%s", "%s", "%s", %d);' % (game_id, content, DateUtil.lomgToStrTime(time / 1000), author, score))
-                # cur.execute('INSERT INTO comment(game_id, content, comment_time, author, score) '
-                #             'VALUES ("%s", "%s", "%s", "%s", %d);' % (id, content, time, author, score))
+                      'VALUES ("%s", "%s", "%s", "%s", %d);' % (
+                          game_id, content, DateUtil.lomgToStrTime(time / 1000), author, score))
+                cur.execute('INSERT INTO comment(game_id, content, comment_time, author, score) '
+                            'VALUES ("%s", "%s", "%s", "%s", %d);' % (
+                                game_id, content, DateUtil.lomgToStrTime(time / 1000), author, score))
             except:
+                print(sys.exc_info()[0], ":", sys.exc_info()[1])
                 pass
         page += 1
         hasMore = result['hasMore']
+
+    conn.commit()
+    DbUtil.close(conn, cur)
 
 
 if __name__ == '__main__':
