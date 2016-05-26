03e57ebc7d04165695f5a9f019164bf390035734
bzz <whiteevilban@gmail.com>
2016-05-26 16:23:03 +0800

    360

--- site/mybzz/crawler/360Crawler.py
+++ site/mybzz/crawler/360Crawler.py
@@ -1,9 +1,10 @@
+import sys
+
 from site.mybzz.util import DbUtil
 from site.mybzz.util import BsUtil
 from site.mybzz.util import DateUtil
 
-
-# conn, cur = DbUtil.getConn()
+conn, cur = DbUtil.getConn()
 
 
 def getData(name, id, score, totalDownload):
@@ -13,30 +14,47 @@ def getData(name, id, score, totalDownload):
     totalComCount = result['data']['total']
 
     print('INSERT INTO games(game_name,from_store, total_comment_count, total_score, total_download, data_date) '
-          'VALUES ("%s", "%s", "%s", "%s", "%s", "%s");' % (
-              name, '360', totalComCount, score,
+          'VALUES ("%s", "%s", "%s", %d, "%s", "%s");' % (
+              name, '360', totalComCount, (score * 10) / 2,
               totalDownload, DateUtil.currentDate()))
+    cur.execute('INSERT INTO games(game_name,from_store, total_comment_count, total_score, total_download, data_date) '
+                'VALUES ("%s", "%s", "%s", %d, "%s", "%s");' % (
+                    name, '360', totalComCount, (score * 10) / 2,
+                    totalDownload, DateUtil.currentDate()))
+    game_id = cur.lastrowid
 
     while (True):
-        result = BsUtil.praseJson(commentUrl % (id, start, count))
+        try :
+            result = BsUtil.praseJson(commentUrl % (id, start, count))
+        except:
+            print(commentUrl % (id, start, count))
         if not result['data']['messages']:
             break
         for comment in result['data']['messages']:
-            print(comment['username'], comment['content'], comment['score'], comment['create_time'])
+            # print(comment['username'], comment['content'], comment['score'], comment['create_time'])
             print('INSERT INTO comment(game_id, content, comment_time, author, score) '
                   'VALUES ("%s", "%s", "%s", "%s", %s);' % (
-                      0, comment['content'].replace('\n', ''), comment['create_time'],
+                      game_id, comment['content'].replace('\n', ''), comment['create_time'],
                       comment['username'], comment['score']))
+            try:
+                cur.execute('INSERT INTO comment(game_id, content, comment_time, author, score) '
+                        'VALUES ("%s", "%s", "%s", "%s", %s);' % (
+                            game_id, comment['content'].replace('\n', '').replace('\"','\''), comment['create_time'],
+                            comment['username'], comment['score']))
+            except:
+                print(sys.exc_info()[0], ":", sys.exc_info()[1])
+                pass
         start += 50
 
 
 def getTop50():
-    result = BsUtil.praseGzipJson("http://openbox.mobilem.360.cn/app/rank?from=game&type=download&startCount=1")
-    print(result)
-    for app in result['data']:
+    result = BsUtil.praseJson("http://openbox.mobilem.360.cn/app/rank?from=game&type=download&startCount=1")
+    # print(result)
+    for app in result['data'][4:]:
         print(app['name'], app['id'], app['rating'], app['download_times'])
-        getData(app['name'], app['id'], app['rating'], app['download_times'])
-
+        getData(app['name'], app['id'], float(app['rating']), app['download_times'])
+        conn.commit()
 
 if __name__ == '__main__':
     getTop50()
+    DbUtil.close(conn, cur)
---
+++ site/mybzz/crawler/TencentCrawler.py
@@ -0,0 +1,16 @@
+from site.mybzz.util import DbUtil
+from site.mybzz.util import BsUtil
+from site.mybzz.util import DateUtil
+
+
+def getData():
+    pass
+
+
+def getTop():
+    getData()
+    pass
+
+
+if __name__ == '__main__':
+    getTop()
