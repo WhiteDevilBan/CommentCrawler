bf7272553584b2c8f5694b8a33890c5570901a77
bzz <whiteevilban@gmail.com>
2016-05-27 13:48:24 +0800

    qq

--- site/mybzz/crawler/TencentCrawler.py
+++ site/mybzz/crawler/TencentCrawler.py
@@ -1,16 +1,53 @@
+import sys
+
 from site.mybzz.util import DbUtil
 from site.mybzz.util import BsUtil
 from site.mybzz.util import DateUtil
 
 
-def getData():
-    pass
+def getData(name, downloadCount, score, packageName):
+    contextData = ''
+    url = "http://sj.qq.com/myapp/app/comment.htm?apkName=%s&contextData=%s"
+    result = BsUtil.praseJson(url % (packageName, contextData))
+
+    totalComCount = result['obj']['total']
+    print(
+        'INSERT INTO games(game_name,from_store, total_comment_count, total_score, total_download, data_date) '
+        'VALUES ("%s", "%s", "%s", %d, "%s", "%s");' % (
+            name, 'qq', totalComCount, score, downloadCount, DateUtil.currentDate()))
+
+    while (True):
+        result = BsUtil.praseJson(url % (packageName, contextData))
+        try:
+            if not result['success']:
+                continue
+            if result['obj']['hasNext'] != 1:
+                break
+
+            for comment in result['obj']['commentDetails']:
+                print('INSERT INTO comment(game_id, content, comment_time, author, score) '
+                      'VALUES ("%s", "%s", "%s", "%s", %s);' % (
+                          0, comment['content'].replace('\r', ''), DateUtil.longToStrTime(int(comment['createdTime'])),
+                          comment['nickName'], comment['score']))
+                # print(comment)
+                # if comment['nickName'] == "♀少说多笑]^ω^[":
+                #     print(comment)
+            contextData = result['obj']['contextData']
+        except:
+            print(result)
+            print(sys.exc_info()[0], ":", sys.exc_info()[1])
 
 
 def getTop():
-    getData()
+    url = "http://pngweb.3g.qq.com/getSubRankList?sortType=22&categoryId=-2&pageSize=20&startIndex=0&needCateList=0&phoneGuid=891204461307686912&phoneImei=862095025228963&qua=TMAF_652_F_2152%2F062152%26NA%2F062152%2F6522130_2152%265.1_22_2_0_0%26120_72_14%26Meizu_MX4_Meizu_meizumx4%261000047%262152%26V3&androidId=dba20155a97326c&macAdress=&imsi=460011431632413&wifiBssid="
+    result = BsUtil.praseQQ(url)
+    for app in result['appList']:
+        print(app['appName'], app['apkId'], '下载数：', app['appDownCount'], '评分：', float(app['score']) * 10,
+              app['packageName'])
+        getData(app['appName'], app['appDownCount'], float(app['score']) * 10, app['packageName'])
     pass
 
 
 if __name__ == '__main__':
-    getTop()
+    # getTop()
+    getData('', 0, 0, packageName='com.qqgame.hlddz')
---
+++ site/mybzz/test/TestQQ.py
@@ -0,0 +1,24 @@
+from urllib import request
+
+import zlib
+
+req = request.Request("http://pngweb.3g.qq.com/getSubRankList?sortType=22&categoryId=0&pageSize=20&"
+                      "startIndex=0&needCateList=1")
+
+req.add_header("Referer","http://qzs.qq.com/open/yyb/yyb_toplist/html/downtoplist.html?_ck_bid=3")
+req.add_header("Origin","http://qzs.qq.com")
+req.add_header("Accept","text/xml, text/html, application/xhtml+xml, image/png, text/plain, */*;q=0.8")
+req.add_header("User-Agent","Mozilla/5.0 (Linux; Android 5.1; MX4 Build/LMY47I) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/40.0.2214.117 Mobile Safari/537.36/apiLevel/22/qqdownloader/3/ft_apiLevel/1_0_0_0")
+req.add_header("Accept-Language","zh-CN")
+req.add_header("Accept-Charset","utf-8, iso-8859-1, utf-16, *;q=0.7")
+req.add_header("Accept-Encoding","gzip")
+req.add_header("Connection","keep-alive")
+# req.add_header("Host","pngweb.3g.qq.com")
+# req.add_header("Cookie","accesstoken=; caller=13; guid=891204461307686912; imei=862095025228963; isforeground=1; logintype=NONE; openappid=0; openid=; qaccesstoken=null; qopenid=null; sid=; skey=; skey_datetime=; uin=; via=UNKNOWN_VIA; vkey=")
+req.add_header("Q-UA2","QV=2&PL=ADR&PR=TBS&PB=GE&VE=B1&VN=1.5.1.1065&CO=X5&COVN=025489&RF=PRI&PP=com.tencent.android.qqdownloader&PPVC=6522130&RL=1152*1920&MO= MX4 &DE=PHONE&OS=5.1&API=22&CHID=0&LCID=9422")
+req.add_header("Q-GUID","cee926d32c56b35b7aa4310013b788cb")
+req.add_header("Q-Auth","31045b957cf33acf31e40be2f3e71c5217597676a9729f1b")
+
+result = request.urlopen(req)
+# print(result.read())
+print(zlib.decompress(result.read(), 16 + zlib.MAX_WBITS).decode())
