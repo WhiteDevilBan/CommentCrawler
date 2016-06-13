c7283996006c35aa1fc1fc81848f977c35741435
bzz <whiteevilban@gmail.com>
2016-06-13 10:56:15 +0800

    jiaba

--- site/mybzz/test/TestJieBa.py
+++ site/mybzz/test/TestJieBa.py
@@ -1,22 +1,28 @@
-import os
-
 import jieba
-import jieba.posseg as pseg
+import jieba.analyse
+from site.mybzz.util import DbUtil
+
+conn, cur = DbUtil.getConn()
 
 if __name__ == '__main__':
 
-    result = jieba.cut("超爽的视觉盛宴，真实还原端游的质感，一万个赞")
-
-    f = open("../StopWords.txt",encoding="utf-8")
+    f = open("../StopWords.txt", encoding="utf-8")
     l = []
     while True:
-        line = f.readline().replace("\n",'')
-        # print(line)
+        line = f.readline().replace("\n", '')
+
         if not line:
             break
         l.append(line)
-    # print(l)
-    for seg in result:
-        if seg not in l:
-            print(seg)
 
+    comments = DbUtil.getAllResult("select * from comment limit 100 offset 383721")
+    for comment in comments:
+
+        print(comment)
+        result = jieba.cut(comment[2])
+
+        for seg in result:
+            if seg not in l:
+                print(seg)
+
+        print(",".join(jieba.analyse.extract_tags(comment[2], topK=20)))
